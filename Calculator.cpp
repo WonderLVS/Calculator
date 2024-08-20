5b25caad29c79b91f4dfece426c05fb4e900fe4e
Voldemar <uss.agent01@gmail.com>
2024-08-20 12:23:45 +0300

    put arithmetic in its own class, and make Calculator.exe file work properly (program closing after first calculation)

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
-int main() 
+void arithmetics() 
 {
-    int num1 = 0;
-    int num2 = 0;
-    int result = 0;
+    float num1 = 0;
+    float num2 = 0;
+    float result = 0;
     int choose = 0;
 
     cout  << "Please, enter the first number" << endl;
@@ -15,27 +16,50 @@ int main()
     cout << "Your first number is: " << num1 << "\nPlease, enter the second number" << endl;
     cin >> num2;
 
-    cout << "Your first number is: " << num1 << "\nYour second number is: " << num2 << "\nPlease, choose the action you`re going to perform: \n 1. + \n 2. - \n 3. * \n 4. / " << endl;
+    cout << "Your first number is: " << num1 << "\nYour second number is: " << num2 << "\nPlease, choose the action you`re going to perform: \n 1. a + b \n 2. a - b \n 3. a * b \n 4. a / b " << endl;
     cin >> choose;
 
-    switch (choose) 
+    if (choose == 1) 
     {
-        case 1:
-            result = num1 + num2;
-            cout << "Very well, number " << num1 << " plus number " << num2 << " equals " << result << endl;
-            break;
-        case 2: 
-            result = num1 - num2;
-            cout << "Very well, number " << num1 << " minus number " << num2 << " equals " << result << endl;
-            break;
-        case 3:
-            result = num1 * num2;
-            cout << "Very well, number " << num1 << " times number " << num2 << " equals " << result << endl;
-            break;
-        case 4:
-            result = num1 / num2;
-            cout << "Very well, number " << num1 << " divided by number " << num2 << " equals " << result << endl;
-            break;
+        result = num1 + num2;
+        cout << "Very well, number " << num1 << " plus number " << num2 << " equals " << result << endl;
     }
-    
+    else if (choose == 2)
+    {
+        result = num1 - num2;
+        cout << "Very well, number " << num1 << " minus number " << num2 << " equals " << result << endl;
+    }
+    else if (choose == 3)
+    {
+        result = num1 * num2;
+        cout << "Very well, number " << num1 << " times number " << num2 << " equals " << result << endl;
+    }
+    else if (choose == 4) 
+    {
+        result = num1 / num2;
+        cout << "Very well, number " << num1 << " divided by number " << num2 << " equals " << std::fixed << std::setprecision(2) << result << endl;
+    }
+    else 
+    {
+        cout << "Input Error,  please, use the numbers provided to choose " << endl;
+    }
+}
+
+int main() 
+{
+    while(true) 
+    {
+        int choose;
+        cout << "Please, select the type of operation you want to use: \n1. Basic Arithmetic" << endl;
+        cin >> choose;
+
+        if (choose == 1) 
+        {
+            arithmetics();
+        }
+        else 
+        {
+            cout << "Input Error,  please, use the numbers provided to choose " << endl;
+        }
+    }
 }
